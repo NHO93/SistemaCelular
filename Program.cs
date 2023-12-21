6671a7271d6240ab7819b2a83dfeb2aef5344b8f
Edenilson Fabiano <edenilsonsouza.BSI@gmail.com>
2023-12-21 19:46:01 -0300

    Verificando algumas linhas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// DONE
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Nokia nk123 = new Nokia("000145", "NK123", "305460516516051651", 256);
+        nk123.InstalarAplicativo("WhatsApp");
+
+        Iphone iphone5S = new Iphone("145000", "iphone5S", "1561506151615064503", 652);
+        iphone5S.InstalarAplicativo("Instagram");
+    }
+}
