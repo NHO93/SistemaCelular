e575d412b6ffd69dd05c1226c38ec00a542d7558
Edenilson <20231ITA01GB0011@alunos.ifbaiano.edu.br>
2023-12-29 16:38:20 -0300

    Atualizando umas linhas
    Adicionando mais algumas linhas

--- Program.cs
+++ Program.cs
@@ -6,10 +6,16 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Nokia nk123 = new Nokia("000145", "NK123", "305460516516051651", 256);
-        nk123.InstalarAplicativo("WhatsApp");
+        Console.WriteLine("Nokia\n");
+        Nokia nk123 = new Nokia("000145", "NK123", "305460516516051651", 256); // TODO: Instanciar um Nokia
+        nk123.InstalarAplicativo("WhatsApp"); //  TODO: Instalar o WhatsApp
+        nk123.Ligar(); //   TODO: Ligar
+        nk123.ReceberLigacao(); //  TODO: Receber uma ligação
 
-        Iphone iphone5S = new Iphone("145000", "iphone5S", "1561506151615064503", 652);
-        iphone5S.InstalarAplicativo("Instagram");
+        Console.WriteLine("\nIphone\n");
+        Iphone iphone5S = new Iphone("145000", "iphone5S", "1561506151615064503", 652); // TODO: Instanciar um Iphone
+        iphone5S.InstalarAplicativo("Instagram"); //    TODO: Instalar o Instagram
+        iphone5S.Ligar(); // TODO: Ligar
+        iphone5S.ReceberLigacao(); //   TODO: Receber uma ligação
     }
 }
